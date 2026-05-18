210e947425213c598a685019416c77887dc4e197
Sebastiao Matusse <matussevalete@gmail.com>
2026-05-18 12:57:32 +0200

    comment my code part and let the incoming code from brittney as the executable one

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -7,22 +7,6 @@ export default class ProductDatails {
         this.product = {};
         this.dataSource = dataSource;
     };
-    // async init() {
-    //     // use the datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
-    //     this.product = await this.dataSource.findProductById(this.product);
-
-    //     // the product details are needed before rendering the HTML
-    //     this.renderProductDetails();
-
-    //     // once the HTML is rendered, add a listener to the Add to Cart button
-    //     document.getElementById('addToCart')
-    //         .addEventListener('click', this.addToCart.bind(this));
-
-    //     // Notice the .bind(this). This callback will not work if the bind(this) is missing. Review the readings from this week on 'this' to understand why.
-
-
-
-    // }
     async init() {
         // get product details from data source
         this.product = await this.dataSource.findProductById(this.productId);
@@ -42,29 +26,6 @@ export default class ProductDatails {
 
         setLocalStorage("so-cart", cartItems);
     }
-
-    // renderProductDetails() {
-    //     // generate and populae the HTML to display the product details
-    //     const tenplate = document.querySelector("product-card")
-    //     const productList = document.querySelector("product-list")
-
-    //     products.forEach(product => {
-    //         const clone = template.content.cloneNode(true);
-    //         const [title, productName, img, price, color, description, addToCartBtn] = clone.querySelectorAll("h3, h2, img, p, p, p, button")
-
-    //         title.textcontent = this.product.Brand.Name;
-    //         productName.textcontent = this.product.NameWithoutBrand
-    //         img.src = this.product.Image
-    //         img.alt = this.product.NameWithoutBrand
-    //         price.textcontent = this.product.FinalPrice
-    //         color.textcontent = this.product.Colors.ColorName
-    //         description.textcontent = this.product.DescriptionHtmlSimple
-    //         addToCartBtn.dataset = this.productId
-
-    //         productList.appendChild(clone);
-    //     });
-
-    // };
 
 
     renderProductDetails() {
@@ -93,3 +54,45 @@ export default class ProductDatails {
             this.product.DescriptionHtmlSimple;
     }
 }
+
+
+    // async init() {
+    //     // use the datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+    //     this.product = await this.dataSource.findProductById(this.product);
+
+    //     // the product details are needed before rendering the HTML
+    //     this.renderProductDetails();
+
+    //     // once the HTML is rendered, add a listener to the Add to Cart button
+    //     document.getElementById('addToCart')
+    //         .addEventListener('click', this.addToCart.bind(this));
+
+    //     // Notice the .bind(this). This callback will not work if the bind(this) is missing. Review the readings from this week on 'this' to understand why.
+
+
+
+    // }
+
+        // renderProductDetails() {
+    //     // generate and populae the HTML to display the product details
+    //     const tenplate = document.querySelector("product-card")
+    //     const productList = document.querySelector("product-list")
+
+    //     products.forEach(product => {
+    //         const clone = template.content.cloneNode(true);
+    //         const [title, productName, img, price, color, description, addToCartBtn] = clone.querySelectorAll("h3, h2, img, p, p, p, button")
+
+    //         title.textcontent = this.product.Brand.Name;
+    //         productName.textcontent = this.product.NameWithoutBrand
+    //         img.src = this.product.Image
+    //         img.alt = this.product.NameWithoutBrand
+    //         price.textcontent = this.product.FinalPrice
+    //         color.textcontent = this.product.Colors.ColorName
+    //         description.textcontent = this.product.DescriptionHtmlSimple
+    //         addToCartBtn.dataset = this.productId
+
+    //         productList.appendChild(clone);
+    //     });
+
+    // };
+
