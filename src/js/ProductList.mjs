dfd8b00d26696c4645ba9b84a8745075ce44750f
Sebastiao Matusse <matussevalete@gmail.com>
2026-05-19 14:09:14 +0200

    !!! Message generated by Ai!!!
    Refactors product display to dynamic rendering
    
    Updates product listings and detail pages to use dynamic data fetching and rendering. Introduces ProductList and ProductData modules for handling product information. Consolidates individual product pages into a single dynamic template, removing numerous obsolete static image and JSON data files. Adds a utility function for template-based list rendering and a `vite preview` script for local build testing.

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,39 @@
+function productCardTemplate(product) {
+    return `
+    <li class="product-card">
+    <a href="product_pages/?product=${product.Id}">
+      <img src="${product.Image}" alt="Image of ${product.Name}">
+      <h2 class="card__brand">${product.Brand.Name}</h2>
+      <h3 class="card__name">${product.Name}</h3>
+      <p class="product-card__price">$${product.FinalPrice}</p>
+    </a>
+  </li>`;
+}
+
+export default class ProductList {
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        const productList = await this.dataSource.getData();
+        this.renderList(productList)
+        console.log(productList)
+
+    }
+
+    renderList(list) {
+
+        this.renderListWithTemplate(productCardTemplate, this.listElement, list);
+
+    }
+}
+
+
+// function renderCartContents() {
+//     const cartItems = getLocalStorage("so-cart");
+//     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+//     document.querySelector(".product-list").innerHTML = htmlItems.join("");
+// }
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import ProductList from "./ProductList.mjs";
+import ProductData from "./ProductData.mjs";
+
+element = document.querySelector(".prduct-list");
+
+const dataSource = new ProductData("tents");
+const productList = new ProductList("tents", dataSource, element);
+
+productList.init();
