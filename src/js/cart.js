f339941591b71c7a4e2726a9070f14f72659a800
Sebastiao Matusse <matussevalete@gmail.com>
2026-05-24 04:54:08 +0200

    Minor details conflicts commented on in the cart.js file, check it out please

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, loadHeaderFooter } from "./utils.mjs";
 import CartProductList from "./ShoppingCart.mjs";
 
 const cartListElement = document.querySelector(".product-list")
@@ -7,33 +7,8 @@ cartItemList.init()
 
 loadHeaderFooter();
 
-// function renderCartContents() {
-//   const cartItems = getLocalStorage("so-cart");
-//   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-//   document.querySelector(".product-list").innerHTML = htmlItems.join("");
-// }
 
-// function cartItemTemplate(item) {
-//   const newItem = `<li class="cart-card divider">
-//   <a href="#" class="cart-card__image">
-//     <img
-//       src="${item.Image}"
-//       alt="${item.Name}"
-//     />
-//   </a>
-//   <a href="#">
-//     <h2 class="card__name">${item.Name}</h2>
-//   </a>
-//   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-//   <p class="cart-card__quantity">qty: 1</p>
-//   <p class="cart-card__price">$${item.FinalPrice}</p>
-// </li>`;
-
-//   return newItem;
-// }
-
-// // renderCartContents();
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+// renderCartContents();
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
@@ -63,8 +38,10 @@ function cartItemTemplate(item) {
   return newItem;
 }
 
-renderCartContents();
+renderCartContents(); // The renderListWithTemplate from Utils is already rendering content in shppingCart.mjs
 
+
+// This removes all the  products with the same id. I think it should remove one product at a time
 async function removeFromCart(e) {
   const productId = e.target.dataset.id;
   const cartItems = getLocalStorage("so-cart") || [];
