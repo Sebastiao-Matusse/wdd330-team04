693be4f79ba6c720b3eed5f493b14eda223c553d
1vadim <vchemariev@byupathway.edu>
2026-05-26 12:25:19 +0200

    vc-w3-ind activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
-import ProductList from "./ProductList.mjs";
-import ProductData from "./ProductData.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
-const element = document.querySelector(".product-list");
+loadHeaderFooter();
 
-const dataSource = new ProductData("tents");
-const productList = new ProductList("tents", dataSource, element);
+// import ProductList from "./ProductList.mjs";
+// import ProductData from "./ProductData.mjs";
 
-productList.init();
+// const element = document.querySelector(".product-list");
+
+// const dataSource = new ProductData("tents");
+// const productList = new ProductList("tents", dataSource, element);
+
+// productList.init();
---
+++ src/js/product-listing.js
@@ -0,0 +1,21 @@
+import { loadHeaderFooter } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+loadHeaderFooter();
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const category = urlParams.get("category") || "tents"; 
+
+
+const titleElement = document.querySelector(".category-title");
+if (titleElement) {
+  titleElement.textContent = `Top ${category.charAt(0).toUpperCase() + category.slice(1)}`;
+}
+
+const listElement = document.querySelector(".product-list");
+const dataSource = new ProductData(category); 
+const productList = new ProductList(category, dataSource, listElement);
+
+productList.init();
