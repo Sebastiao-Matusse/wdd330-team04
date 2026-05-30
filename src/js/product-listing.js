d14560b02dbf58e296a021a254a37c28dddc719a
Nedie Yassin <yassinnedie9121@gmail.com>
2026-05-31 01:27:52 +0200

    Product List sort
    On the product list page add the ability to sort by name or price.

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -4,13 +4,12 @@ import { getParam, loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-
-
-
 const element = document.querySelector(".product-list");
+const sortInput = document.getElementById("product-sort-input");
 const category = getParam("category");
 const dataSource = new ProductData();
-const productList = new ProductList(category, dataSource, element);
+const productList = new ProductList(category, dataSource, element, sortInput);
 
 document.getElementById("pro-cat").innerHTML = category.replace("-", " ");
 productList.init();
+productList.initSort();
