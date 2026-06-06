3bf71a00cd9eb046e24cb94cf7b765a32054ecae
1vadim <vchemariev@byupathway.edu>
2026-06-06 18:22:16 +0200

    W04 Team Activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,5 @@
 import ProductList from "./ProductList.mjs";
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
-
-const element = document.querySelector(".product-list");
-
-const dataSource = new ProductData("tents");
-const productList = new ProductList("tents", dataSource, element);
-
-productList.init();
